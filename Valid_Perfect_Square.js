2746d8dea9def99125834a208d86866c8a5a9b79
RAJ VIKRAM SINGH <38528859+Raj-Vikram-Singh@users.noreply.github.com>
2020-05-09 23:28:57 +0530

    Create Valid_Perfect_Square.js

---
+++ Valid_Perfect_Square.js
@@ -0,0 +1,38 @@
+// =====================    Problem Statement =================================================================
+
+/*
+Given a positive integer num, write a function which returns True if num is a perfect square else False.
+
+Note: Do not use any built-in library function such as sqrt.
+
+Example 1:
+
+Input: 16
+Output: true
+Example 2:
+
+Input: 14
+Output: false
+*/
+
+
+
+
+//=================================================  Solution =================================================
+
+
+
+
+/**
+ * @param {number} num
+ * @return {boolean}
+ */
+// Method 1:
+
+var isPerfectSquare = function(num) {
+   if ((Math.sqrt(num)*10) % 10 === 0){
+       return true;
+   }
+    return false;
+};
+
